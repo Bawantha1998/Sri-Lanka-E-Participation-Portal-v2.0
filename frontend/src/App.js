895aec3ff44bb9878f2e0bfdf8a78bb0ee78f86e
it21232336 <it21232336@my.sliit.lk>
2024-02-02 15:50:07 +0530

    public consultations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,26 @@
-
 // import Home from "./components/card";
-// import Newspress from "./pages/Newspress";
-import Event from "./components/Event/Eventcard"
-
+import Newspress from "./pages/Newspress";
 import Allimagegallery from "./components/pages/allimagegallery";
-import Home from "./components/card";
-
 import Photogallery from "./components/pages/photogallery";
+import PublicConsultations from "./pages/PublicConsultations";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 
 function App() {
   return (
     <div>
-     <BrowserRouter>
-     <Routes>
-     <Route path='/' element={<Photogallery/>} />
-     <Route path='/Allimages' element={<Allimagegallery/>} />
-     </Routes>
-     </BrowserRouter>
-     
-      {/* <Home /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Photogallery />} />
+          <Route path="/newspress" element={<Newspress />} />
+          <Route
+            path="/public-consultations"
+            element={<PublicConsultations />}
+          />
+          <Route path="/Allimages" element={<Allimagegallery />} />
+        </Routes>
+      </BrowserRouter>
 
+      {/* <Home /> */}
     </div>
   );
 }
--- frontend/src/components/publicConsultations/PublicConsultationsCard.jsx
+++ frontend/src/components/publicConsultations/PublicConsultationsCard.jsx
@@ -1,5 +1,53 @@
-import React from "react";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Imgs from "../../images/8.jpg";
 
 export default function PublicConsultationsCard() {
-  return <div></div>;
+  const handleExpandClick = () => {};
+
+  return (
+    <Card sx={{ maxWidth: 350 }}>
+      <CardHeader
+        title={
+          <div style={{ fontSize: "20px" }}>
+            <b> Guidelines for Digital Signatures</b>
+          </div>
+        }
+        subheader="September 14, 2016"
+      />
+      <CardMedia component="img" height="194" image={Imgs} alt="Paella dish" />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Guidelines for Digital Signatures" is a key milestone in ICTA policy
+          roadmap, which is already being approved and recognized by the
+          government for implementation. The guidelines aim to direct the
+          government staff towards the adoption of a digital signature to
+          authenticate official documents. The effective usage of digital..
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          sx={{
+            bgcolor: "#345AE3",
+            color: "#FFFFFF",
+            fontSize: 13,
+            fontWeight: "",
+            "&:hover": {
+              bgcolor: "#2a487e", // Change this to the desired hover color
+            },
+          }}
+          size="small"
+        >
+          View More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
