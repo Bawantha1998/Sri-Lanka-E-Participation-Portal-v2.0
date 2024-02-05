53af51bcc19fac06c66e93e38f4e0205aec5c9ac
Athapaththu <sakila.atapattu@gmail.com>
2024-02-05 14:17:05 +0530

    gallery view card change

--- frontend/src/pages/PublicConsultations.jsx
+++ frontend/src/pages/PublicConsultations.jsx
@@ -10,16 +10,8 @@ export default function PublicConsultations() {
         <Grid container item sm={12} md={12}>
           <PublicConsultationbreadcrumb />
         </Grid>
-        <Grid item md={3} sm={6} xs={12}>
-          <Box marginLeft={3.5} marginTop={1}>
-            <PublicConsultationsCard />
-          </Box>
-        </Grid>
-        <Grid item md={3} sm={6} xs={12}>
-          <Box marginLeft={3.5} marginTop={1}>
-            <PublicConsultationsCard />
-          </Box>
-        </Grid>
+        
+       
         <Grid item md={3} sm={6} xs={12}>
           <Box marginLeft={3.5} marginTop={1}>
             <PublicConsultationsCard />
