7f2b11b42e08257511919f31ba31a645669ce21d
Bawantha1998 <bawanthaprampd918@outlook.com>
2024-02-05 13:49:56 +0530

    app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
-import Home from "./components/card";
+
+
+import Newspress from "./pages/Newspress";
+import Allimagegallery from "./pages/allimagegallery";
+import Photogallery from "./pages/photogallery";
+import PublicConsultations from "./pages/PublicConsultations";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+ 
+
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Photogallery />} />
+          <Route path="/newspress" element={<Newspress />} />
+          <Route
+            path="/public-consultations"
+            element={<PublicConsultations />}
+          />
+          <Route path="/Allimages" element={<Allimagegallery />} />
+        </Routes>
+      </BrowserRouter>
+
+
+  )
+
+
 }
+
+
+
 
 export default App;
 
