124bd93d61328813bb4d398ca157899060415737
it21232336 <it21232336@my.sliit.lk>
2024-02-13 14:04:08 +0530

    change color

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,107 +1,113 @@
-import React from 'react';
-import { Grid, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import {
-    FaDribbbleSquare,
-    FaFacebookSquare,
-    FaGithubSquare,
-    FaInstagram,
-    FaTwitterSquare,
-} from 'react-icons/fa';
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 // Define styled components for styling
-const RootContainer = styled('div')({
-    backgroundColor: '#1e88e5',
-    maxWidth: '100%',
-    margin: 'auto',
-    padding: '40px',
-    color: '#ffffff',
-    
+const RootContainer = styled("div")({
+  backgroundColor: "#0178ba",
+  maxWidth: "100%",
+  margin: "auto",
+  padding: "40px",
+  color: "#ffffff",
 });
 
-const IconContainer = styled('div')({
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '75%',
-    margin: 'auto',
+const IconContainer = styled("div")({
+  display: "flex",
+  justifyContent: "space-between",
+  width: "75%",
+  margin: "auto",
 });
 
-const SectionContainer = styled('div')({
-    marginBottom: '24px',
+const SectionContainer = styled("div")({
+  marginBottom: "24px",
 });
 
 const Footer = () => {
-    return (
-        <>
-        <br />
-         <RootContainer>
-            
-            <Grid container spacing={4}>
-                <Grid item xs={12} lg={4}>
-                    <Typography variant="h6" gutterBottom>eParticipation Portal | Government of Sri Lanka</Typography>
-                    <Typography variant="body1" gutterBottom>Lorem ipsum dolor, , illum sequi sapiente, quae ullam voluptatem? Harum, error debitis?</Typography>
-                    <IconContainer>
-                        <FaFacebookSquare size={30}/>
-                        <FaGithubSquare size={30}/>
-                        <FaInstagram size={30}/>
-                        <FaTwitterSquare size={30}/>
-                        <FaDribbbleSquare size={30}/>
-                    </IconContainer>
-                </Grid>
-                <Grid item xs={12} lg={8} container spacing={4}>
-                    <Grid item xs={12} md={6} lg={3} component={SectionContainer}>
-                        <Typography variant="h6" gutterBottom>Participate</Typography>
-                        <ul>
-                            <li>Who We Are</li>
-                            <li>Mandate</li>
-                            <li>Vision</li>
-                            <li>Mission</li>
-                            <li>Company Structure</li>
-                         
-                        </ul>
-                    </Grid>
-                    {/* Other Grid items go here */}
-                    <Grid item xs={12} md={6} lg={3} component={SectionContainer}>
-                        <Typography variant="h6" gutterBottom>About Us</Typography>
-                        <ul>
-                            <li>Who We Are</li>
-                            <li>Mandate</li>
-                            <li>Vision</li>
-                            <li>Mission</li>
-                            <li>Company Structure</li>
-                           
-                        </ul>
-                    </Grid>
-
-                    <Grid item xs={12} md={6} lg={3} component={SectionContainer}>
-                        <Typography variant="h6" gutterBottom>User Policies</Typography>
-                        <ul>
-                            <li>Who We Are</li>
-                            <li>Mandate</li>
-                            <li>Vision</li>
-                            <li>Mission</li>
-                            <li>Company Structure</li>
-                            
-                        </ul>
-                    </Grid>
-
-                        <Grid item xs={12} md={6} lg={3} component={SectionContainer}>
-                        <Typography variant="h6" gutterBottom>For Media</Typography>
-                        <ul>
-                            <li>Who We Are</li>
-                            <li>Mandate</li>
-                            <li>Vision</li>
-                            <li>Mission</li>
-                            <li>Company Structure</li>
-                           
-                        </ul>
-                    </Grid>
-                </Grid>
+  return (
+    <>
+      <br />
+      <RootContainer>
+        <Grid container spacing={4}>
+          <Grid item xs={12} lg={4}>
+            <Typography variant="h6" gutterBottom>
+              eParticipation Portal | Government of Sri Lanka
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Lorem ipsum dolor, , illum sequi sapiente, quae ullam voluptatem?
+              Harum, error debitis?
+            </Typography>
+            <IconContainer>
+              <FaFacebookSquare size={30} />
+              <FaGithubSquare size={30} />
+              <FaInstagram size={30} />
+              <FaTwitterSquare size={30} />
+              <FaDribbbleSquare size={30} />
+            </IconContainer>
+          </Grid>
+          <Grid item xs={12} lg={8} container spacing={4}>
+            <Grid item xs={12} md={6} lg={3} component={SectionContainer}>
+              <Typography variant="h6" gutterBottom>
+                Participate
+              </Typography>
+              <ul>
+                <li>Who We Are</li>
+                <li>Mandate</li>
+                <li>Vision</li>
+                <li>Mission</li>
+                <li>Company Structure</li>
+              </ul>
             </Grid>
-        </RootContainer>
-        </>
-       
-    );
-}
+            {/* Other Grid items go here */}
+            <Grid item xs={12} md={6} lg={3} component={SectionContainer}>
+              <Typography variant="h6" gutterBottom>
+                About Us
+              </Typography>
+              <ul>
+                <li>Who We Are</li>
+                <li>Mandate</li>
+                <li>Vision</li>
+                <li>Mission</li>
+                <li>Company Structure</li>
+              </ul>
+            </Grid>
+
+            <Grid item xs={12} md={6} lg={3} component={SectionContainer}>
+              <Typography variant="h6" gutterBottom>
+                User Policies
+              </Typography>
+              <ul>
+                <li>Who We Are</li>
+                <li>Mandate</li>
+                <li>Vision</li>
+                <li>Mission</li>
+                <li>Company Structure</li>
+              </ul>
+            </Grid>
+
+            <Grid item xs={12} md={6} lg={3} component={SectionContainer}>
+              <Typography variant="h6" gutterBottom>
+                For Media
+              </Typography>
+              <ul>
+                <li>Who We Are</li>
+                <li>Mandate</li>
+                <li>Vision</li>
+                <li>Mission</li>
+                <li>Company Structure</li>
+              </ul>
+            </Grid>
+          </Grid>
+        </Grid>
+      </RootContainer>
+    </>
+  );
+};
 
 export default Footer;
--- frontend/src/components/gallery/gallerybreadcrumb.jsx
+++ frontend/src/components/gallery/gallerybreadcrumb.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
@@ -6,67 +6,64 @@ import Stack from "@mui/material/Stack";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { Box, Container } from "@mui/material";
 
-
-
-
 function handleClick(event) {
-    event.preventDefault();
-    console.info("You clicked a breadcrumb.");
-  }
+  event.preventDefault();
+  console.info("You clicked a breadcrumb.");
+}
 export default function Gallerybreadcrumb() {
-    const breadcrumbs = [
-        <Link
-          underline="hover"
-          key="1"
-          color="#FFFFFF"
-          href="/"
-          onClick={handleClick}
-        >
-          Home
-        </Link>,
-        // <Link
-        //   underline="hover"
-        //   key="2"
-        //   color="#FFFFFF"
-        //   href="/material-ui/getting-started/installation/"
-        //   onClick={handleClick}
-        // >
-        //   Photo Ga
-        // </Link>,
-        <Typography key="3" color="#FFFFFF">
-          Photo Gallery 
-        </Typography>,
-      ];
-    
-      return (
-        <Box sx={{ bgcolor: "#345AE3", width: "100%", height: "auto" }}>
-          <Box marginLeft={3.5} marginTop={2}>
-            <Stack spacing={2}>
-              <Box>
-                <Breadcrumbs
-                  separator={
-                    <NavigateNextIcon
-                      fontSize="small"
-                      style={{ color: "#FFFFFF", fontFamily: "" }}
-                    />
-                  }
-                  aria-label="breadcrumb"
-                >
-                  {breadcrumbs}
-                </Breadcrumbs>
-                <Box>
-                  <Typography
-                    sx={{ fontWeight: { xs: "normal", md: "bold" } }}
-                    key="3"
-                    color="#FFFFFF"
-                    fontSize={{ xs: 30, md: 50 }}
-                  >
-                      Photo Gallery 
-                  </Typography>
-                </Box>
-              </Box>
-            </Stack>
+  const breadcrumbs = [
+    <Link
+      underline="hover"
+      key="1"
+      color="#FFFFFF"
+      href="/"
+      onClick={handleClick}
+    >
+      Home
+    </Link>,
+    // <Link
+    //   underline="hover"
+    //   key="2"
+    //   color="#FFFFFF"
+    //   href="/material-ui/getting-started/installation/"
+    //   onClick={handleClick}
+    // >
+    //   Photo Ga
+    // </Link>,
+    <Typography key="3" color="#FFFFFF">
+      Photo Gallery
+    </Typography>,
+  ];
+
+  return (
+    <Box sx={{ bgcolor: "#0178ba", width: "100%", height: "auto" }}>
+      <Box marginLeft={3.5} marginTop={2}>
+        <Stack spacing={2}>
+          <Box>
+            <Breadcrumbs
+              separator={
+                <NavigateNextIcon
+                  fontSize="small"
+                  style={{ color: "#FFFFFF", fontFamily: "" }}
+                />
+              }
+              aria-label="breadcrumb"
+            >
+              {breadcrumbs}
+            </Breadcrumbs>
+            <Box>
+              <Typography
+                sx={{ fontWeight: { xs: "normal", md: "bold" } }}
+                key="3"
+                color="#FFFFFF"
+                fontSize={{ xs: 30, md: 50 }}
+              >
+                Photo Gallery
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-);
+        </Stack>
+      </Box>
+    </Box>
+  );
 }
