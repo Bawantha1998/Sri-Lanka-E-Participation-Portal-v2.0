8f58a1447f4fac9d625f65b3a900b521beab7ce4
it21232336 <it21232336@my.sliit.lk>
2024-02-16 10:46:57 +0530

    pub card

--- frontend/src/components/publicConsultations/PublicConsultationsCard.jsx
+++ frontend/src/components/publicConsultations/PublicConsultationsCard.jsx
@@ -4,6 +4,7 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
+import { motion } from "framer-motion";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { API_IMG_URL } from "../../utils/constants";
@@ -12,6 +13,10 @@ import { Parser } from "html-to-react";
 
 import { Link } from "react-router-dom";
 
+const cardVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+};
 
 export default function PublicConsultationsCard({
   title,
@@ -22,64 +27,66 @@ export default function PublicConsultationsCard({
 }) {
   const [hovered, setHovered] = React.useState(false);
   return (
-    <Card
-      sx={{
-        maxWidth: 350,
-        "&:hover": {
-          bgcolor: hovered ? "#D3D3D3" : "#",
-        },
-      }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <CardHeader
-        titleTypographyProps={{
-          variant: "h6",
-          style: { lineHeight: "1", minHeight: "4em", fontWeight: "bold" },
+    <motion.div initial="hidden" animate="visible" variants={cardVariants}>
+      <Card
+        sx={{
+          maxWidth: 350,
+          "&:hover": {
+            bgcolor: hovered ? "#D3D3D3" : "#",
+          },
         }}
-        title={title}
-        subheader={subheader}
-      />
-      <CardMedia
-        component="img"
-        maxWidth="160px"
-        height="90px"
-        src={`${API_IMG_URL}/${image}`} // Accessing image from props
-        alt={title}
-      />
-      <CardContent>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          style={{
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            display: "-webkit-box",
-            WebkitLineClamp: 3,
-            WebkitBoxOrient: "vertical",
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <CardHeader
+          titleTypographyProps={{
+            variant: "h6",
+            style: { lineHeight: "1", minHeight: "4em", fontWeight: "bold" },
           }}
-        >
-          {new Parser().parse(description)}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Link to={url}>
-        <Button
-          sx={{
-            bgcolor: "#345AE3",
-            color: "#FFFFFF",
-            fontSize: 13,
-            fontWeight: "",
-            "&:hover": {
-              bgcolor: "#2a487e",
-            },
-          }}
-          size="small"
-        >
-          View More
-        </Button>
-        </Link>
-      </CardActions>
-    </Card>
+          title={title}
+          subheader={subheader}
+        />
+        <CardMedia
+          component="img"
+          maxWidth="160px"
+          height="90px"
+          src={`${API_IMG_URL}/${image}`} // Accessing image from props
+          alt={title}
+        />
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            style={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              display: "-webkit-box",
+              WebkitLineClamp: 3,
+              WebkitBoxOrient: "vertical",
+            }}
+          >
+            {new Parser().parse(description)}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Link to={url}>
+            <Button
+              sx={{
+                bgcolor: "#345AE3",
+                color: "#FFFFFF",
+                fontSize: 13,
+                fontWeight: "",
+                "&:hover": {
+                  bgcolor: "#2a487e",
+                },
+              }}
+              size="small"
+            >
+              View More
+            </Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </motion.div>
   );
 }
